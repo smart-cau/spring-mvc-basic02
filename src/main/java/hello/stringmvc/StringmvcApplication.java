2f4db52d4d361063117682f87dd3baa98d178ae1
HyeonWooJung <1995hyunwoo@gmail.com>
2022-03-12 18:21:31 +0900

    created project

---
+++ src/main/java/hello/stringmvc/StringmvcApplication.java
@@ -0,0 +1,13 @@
+package hello.stringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/stringmvc/StringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.stringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
